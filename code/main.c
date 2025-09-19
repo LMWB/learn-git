d7d12d8f8300cbce152848ce08d404166d8a673e
MaxWilli86 <lukas.bartels@smartcube360.de>
2025-09-19 20:22:29 +0200

    prepare new feature environment

--- code/main.c
+++ code/main.c
@@ -11,11 +11,18 @@ void print_version(void){
     printf("Version %d.%d",VERSION_MAJOR, VERSION_MINOR);
 }
 
+void new_feature(void){
+    ;
+}
+
 int main(void){
 
     print_version();
 
     count_to_ten();
+
+    new_feature();
+    
     while (1)
     {
         ;
