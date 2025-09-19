bead3d2b778470aa3eb19ef025fb39a089846969
MaxWilli86 <lukas.bartels@smartcube360.de>
2025-09-19 20:23:52 +0200

    implement new feature

--- code/main.c
+++ code/main.c
@@ -3,16 +3,18 @@
 
 void count_to_ten(void){
     for(int i = 0; i<10; i++){
-        printf("%d", i+1);
+        printf("%d\n", i+1);
     }
 }
 
 void print_version(void){
-    printf("Version %d.%d",VERSION_MAJOR, VERSION_MINOR);
+    printf("Version %d.%d\n",VERSION_MAJOR, VERSION_MINOR);
 }
 
 void new_feature(void){
-    ;
+    for(int i = 0; i<10; i++){
+        printf("new feature\n");
+    }
 }
 
 int main(void){
@@ -22,7 +24,7 @@ int main(void){
     count_to_ten();
 
     new_feature();
-    
+
     while (1)
     {
         ;
